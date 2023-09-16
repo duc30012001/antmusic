1f15a866000fae14db0bf846adb24754b19890af
duc30012001 <nguyenvietduc3001@gmail.com>
2023-09-16 10:02:34 +0700

    Update background

--- features/aboutUs/introduce.tsx
+++ features/aboutUs/introduce.tsx
@@ -1,4 +1,5 @@
-import bg from '@/assets/ezgif.com-video-to-gif.gif';
+import bg from '@/assets/bgMobile.webp';
+import bgTablet from '@/assets/bgTablet.webp';
 import { NavigateButton } from '@/components/ui';
 import Image from 'next/image';
 
@@ -7,16 +8,25 @@ type Props = {};
 function Introduce({}: Props) {
   return (
     <div className="relative mb-10">
-      <Image src={bg} alt="Loading..." className="aspect-[17/6] w-full" />
-      {/* <video
+      <Image
+        src={bg}
+        alt="Loading..."
+        className="block min-h-[20rem] w-full object-cover sm:hidden"
+      />
+      <Image
+        src={bgTablet}
+        alt="Loading..."
+        className="hidden min-h-[20rem] w-full object-cover sm:block md:hidden"
+      />
+      <video
         loop
         autoPlay
         muted
         id="bg-video"
-        className="min-h-[20rem] object-cover"
+        className="hidden min-h-[20rem] object-cover md:block"
       >
         <source src={'/video.mp4'} type="video/mp4" />
-      </video> */}
+      </video>
       <div className="absolute bottom-2 left-2 right-2 top-2 flex flex-col items-center justify-center">
         <h2 className="section-header mb-1 text-center text-4xl font-bold capitalize leading-tight">
           Unlimited music distribution to all platforms
