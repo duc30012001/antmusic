a332fab85eaa938423f1b819d7bdc56c638980f1
duc30012001 <nguyenvietduc3001@gmail.com>
2023-10-03 10:16:13 +0700

    Update google tag

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/next-script-for-ga */
 import { StyleProvider, createCache, extractStyle } from '@ant-design/cssinjs';
 import type { DocumentContext } from 'next/document';
 import Document, { Head, Html, Main, NextScript } from 'next/document';
@@ -11,7 +12,70 @@ const MyDocument = () => (
         href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap"
         rel="stylesheet"
       />
-      <link rel="icon" href="/logo.png" />
+      <link rel="icon" href="/logo128.png" sizes="128x128" />
+      <link
+        rel="apple-touch-icon-precomposed"
+        href="/logo128.png"
+        sizes="128x128"
+      />
+      <link
+        rel="msapplication-square128x128logo"
+        href="/logo128.png"
+        sizes="128x128"
+      />
+      <link rel="icon" href="/logo64.png" sizes="64x64" />
+      <link
+        rel="apple-touch-icon-precomposed"
+        href="/logo64.png"
+        sizes="64x64"
+      />
+      <link
+        rel="msapplication-square64x64logo"
+        href="/logo64.png"
+        sizes="64x64"
+      />
+      <link rel="icon" href="/logo32.png" sizes="32x32" />
+      <link
+        rel="apple-touch-icon-precomposed"
+        href="/logo32.png"
+        sizes="32x32"
+      />
+      <link
+        rel="msapplication-square32x32logo"
+        href="/logo32.png"
+        sizes="32x32"
+      />
+      <link rel="icon" href="/logo16.png" sizes="16x16" />
+      <link
+        rel="apple-touch-icon-precomposed"
+        href="/logo16.png"
+        sizes="16x16"
+      />
+      <link
+        rel="msapplication-square16x16logo"
+        href="/logo16.png"
+        sizes="16x16"
+      />
+
+      {/* <!-- Meta Tags Generated with https://metatags.io -->
+<!-- Google tag (gtag.js) --> */}
+      <script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-2WWLPS263D"
+      />
+      <script
+        id="google-analytics"
+        dangerouslySetInnerHTML={{
+          __html: `
+                  window.dataLayer = window.dataLayer || [];
+                  function gtag(){dataLayer.push(arguments);}
+                  gtag('js', new Date());
+
+                  gtag('config', 'G-2WWLPS263D');
+                `,
+        }}
+      />
+      {/* <!-- End Google Tag Manager --> */}
     </Head>
     <body>
       <Main />
